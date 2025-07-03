e20d1a048b7643e3e2306149be95077ebc6ee167
l30me55i <87645987+l30me55i@users.noreply.github.com>
2025-07-03 18:56:31 +0530

    Validation added for POST APIs
    Validation added for POST APIs

--- matching-engine/src/main/java/com/baraka/matching_engine/controller/OrdersController.java
+++ matching-engine/src/main/java/com/baraka/matching_engine/controller/OrdersController.java
@@ -1,6 +1,7 @@
 package com.baraka.matching_engine.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +13,7 @@ import com.baraka.matching_engine.dto.Order;
 import com.baraka.matching_engine.request.CreateOrderRequest;
 import com.baraka.matching_engine.service.OrdersService;
 
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
@@ -25,7 +27,7 @@ public class OrdersController {
     private final OrdersService ordersService;
 
     @PostMapping()
-    public ResponseEntity<Order> createOrder(@RequestBody() CreateOrderRequest orderRequest ) {
+    public ResponseEntity<Order> createOrder(@Valid() @RequestBody() CreateOrderRequest orderRequest ) {
         log.info("Request received for creating order for asset {} with price {} and amount {}", orderRequest.getAsset(), orderRequest.getPrice(), orderRequest.getAmount());
         Order order = ordersService.createOrder(orderRequest);
         return ResponseEntity.ok(order);
@@ -33,7 +35,11 @@ public class OrdersController {
 
     @GetMapping("/{id}")
     public ResponseEntity<Order> getOrderDetails(@PathVariable("id") String id) {
-        return ResponseEntity.ok(ordersService.getOrderDetails(id));
+        Order order = ordersService.getOrderDetails(id);
+        if (ObjectUtils.isEmpty(order)) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(order);
         
     }
     
--- matching-engine/src/main/java/com/baraka/matching_engine/request/CreateOrderRequest.java
+++ matching-engine/src/main/java/com/baraka/matching_engine/request/CreateOrderRequest.java
@@ -2,6 +2,10 @@ package com.baraka.matching_engine.request;
 
 import java.math.BigDecimal;
 import com.baraka.matching_engine.enums.Direction;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,9 +14,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CreateOrderRequest {
-
+    @NotNull
     private String asset;
+    @Positive
+    @NotNull
     private BigDecimal price;
+    @Positive
+    @NotNull
     private BigDecimal amount;
     private Direction direction;
 }
